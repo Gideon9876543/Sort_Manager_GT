7b652346212d53e3cc38e93e71a6c9b043829560
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-02-05 16:36:31 +0000

    View of MVC
    The viewer also has logging implemented.

--- src/main/java/com/sparta/IndvProj/view/DisplayManager.java
+++ src/main/java/com/sparta/IndvProj/view/DisplayManager.java
@@ -1,9 +1,14 @@
 package com.sparta.IndvProj.view;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class DisplayManager {
+    private static Logger logger = LogManager.getLogger("Viewer Logger");
+
     public String getDesiredSorter(){
         System.out.print("Please enter your desired sorting method: ");
         Scanner sc = new Scanner(System.in);
@@ -13,6 +18,7 @@ public class DisplayManager {
 
 
     public void displayResults(int[] result){
+        logger.info("Result are shown below: ");
         System.out.println(Arrays.toString(result));
     }
 }
