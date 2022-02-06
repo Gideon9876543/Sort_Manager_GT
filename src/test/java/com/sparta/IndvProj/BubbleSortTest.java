81a97792233ff9ed090ea66b254f44bfb4e9ed9c
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-02-06 15:32:16 +0000

    Test Cases Updated
    The test cases for the Bubble sort algorithm have been completed for positive and negative values.

--- src/test/java/com/sparta/IndvProj/BubbleSortTest.java
+++ src/test/java/com/sparta/IndvProj/BubbleSortTest.java
@@ -4,50 +4,36 @@ import com.sparta.IndvProj.controller.SortManager;
 import com.sparta.IndvProj.model.BubbleSort;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
 //import java.util.Arrays;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BubbleSortTest {
 
+
     @Test
-    public void testSort() {
+    public void testBubbleSortWithNegativeValues() {
         BubbleSort tester = new BubbleSort();
         int[] expected = new int[]{-1, 0, 8};
         assertArrayEquals(expected, tester.sort(new int[]{0, -1, 8}));
 
     }
 
+    @Test
+    @DisplayName("BubbleSort returns a sorted array of some input")
+    public void testBubbleSortWithPositiveValues() {
+        BubbleSort bso = new BubbleSort();
+        int[] tester = {40, 48, 29, 65, 39, 75};
+        assertArrayEquals(new int[]{29, 39, 40, 48, 65, 75}, bso.sort(tester));
+    }
+    
 
 
-
-
-//    private SortManager controller;
-//    @BeforeEach
-//    public void setUp() {
-//        controller = new SortManager();
-//    }
-//
-//    public int[] copyArray(int[] originalArray){
-//        int[] inputArrayCopy = originalArray.clone();
-//        Arrays.sort(inputArrayCopy);
-//        return inputArrayCopy;
-//    }
-//
-//    @Test
-//    public void testBubble() {
-//        int[] inputArray = new int[40];
-//        for(int i = 0; i < inputArray.length; i++){
-//            inputArray[i] = (int)Math.floor((Math.random()*100)+1);
-//        }
-//        int[] inputArrayCopy = copyArray(inputArray);
-//        int[] result = controller.sortArray("b", inputArray);
-//        String expected = Arrays.toString(result);
-//        String actual = Arrays.toString(inputArrayCopy);
-//        Assertions.assertEquals(expected, actual);
-//    }
 }
